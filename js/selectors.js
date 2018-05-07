0a500f6fde519a4e610bc1d7c3e8d5f9686bef18
Gabriel Miranda <gabrielcm.br@gmail.com>
2018-05-07 18:55:11 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,9 @@
-// declare your functions here...
+function paragraphSelector() {
+  return $('p');
+}
+function lastImageSelector() {
+  return $('img:last-child');
+}
+function ninjaBabySelector () {
+  return $('#baby-ninja')
+}
